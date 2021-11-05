b8c2d92301a25515cc5b2c0cf9ed97b8fe9b3ff5
touseef75 <imrantouseef776@gmail.com>
2021-11-05 00:30:43 -0700

    set

--- assets/main.js
+++ assets/main.js
@@ -12,12 +12,14 @@ let img1 = document.getElementById("img1");
 // }
 // hide();
 
-let video1 = document.getElementById("video1")
+let video1 = document.getElementById("video")
 let video2 = document.getElementById("video2")
+let but1 = document.getElementById("but1");
+let but2 = document.getElementById("but2");
 let num =1;
 
  let num2 = 1;
-video1.onclick = function(){
+but1.onclick = function(){
     if(num == 1){
     video1.play();
     num = 0;
@@ -27,13 +29,42 @@ video1.onclick = function(){
         num = 1;
     }
 }
-video2.onclick = function(){
-   if(num2 == 1){
-    video2.play();
-    num2 = 0;
 
+
+
+
+
+
+let arr = [
+    {video : "./images/Osmosis.mp4",
+    color:"#fff8e3"
+},
+    {video : "./images/Osmosis text.mp4",
+    color:"#aae3ff"
+},
+    {video : "./images/salt water label2.mp4",
+    color:"#2e8ebc"
+},
+    {video : "./images/fish.png",
+    color:"black"
+},
+
+]
+
+let box = document.getElementById("box");
+
+let next = document.getElementById("next");
+let num3 = 0;
+next.addEventListener("click",function(){
+    if(num3 == 2){  
+    video1.src = arr[num3].video;
+    box.style.background = arr[num3].color;
+    num3 = 0;
     }else{
-        video2.pause();
-        num2 = 1;
-    };
-}
+        video1.src = arr[num3].video;
+    box.style.background = arr[num3].color;
+    num3++;
+
+    }
+   
+});
